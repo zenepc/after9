2d22b8c25ee692a6d8eece6076949fde4f75fba2
zenepc <zcaneliacik@gmail.com>
2025-10-07 19:39:06 +0300

    debug

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { UserCircle, Menu } from "lucide-react";
+import { Menu } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
